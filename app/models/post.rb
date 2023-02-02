f91385ee674721d61205d0bd4b4d8ab252a8d574
Michael <mikahado@gmail.com>
2023-02-02 00:21:56 -0500

    completes lab

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,25 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+
+    validate :clickbate?
+
+    # below code i had to look up. was not at all in lesson on validations, but learned from it anyway
+
+    # regexp 
+
+    CLICKBAIT_PATTERNS = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+    ]
+
+    def clickbate?
+        if CLICKBAIT_PATTERNS.none? { |p| p.match title }
+            errors.add(:title, "must be clickbait")
+        end
+    end
 end
